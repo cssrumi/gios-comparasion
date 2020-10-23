388b3cda921d725775c469ab3f3b29cfe8e6aac2
Adam Ruminski <cssrumi@gmail.com>
2020-10-23 19:01:55 +0200

    main.py cleaned up

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ from comparator import Comparator
 
 def main():
     station = 'Działoszyn - PM10'
-    from_datetime = datetime(year=2020, month=3, day=20, hour=0, minute=0)
-    to_datetime = datetime(year=2020, month=4, day=30, hour=23, minute=0)
+    from_datetime = datetime(year=2020, month=3, day=1, hour=0, minute=0)
+    to_datetime = datetime(year=2020, month=5, day=30, hour=23, minute=0)
     comparator = Comparator(station, from_datetime, to_datetime)
-    data = comparator\
-        .compare()\
-        .plot()\
-        .data()
-    [print(d.timestamp, d.mongo_value, d.pg_value) for d in data]
+    comparator.compare().plot()
 
 
 if __name__ == '__main__':
